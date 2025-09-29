5ab908ea475022066125d4c673833abfb21bf043
unknown <niccomalds@gmail.com>
2025-09-29 23:38:05 +0800

    add first python file

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+import random
+
+activities = [
+    "Go for a run",
+    "Read a book",
+    "Learn a new Python trick",
+    "Cook something new",
+    "Play a video game",
+    "Do 20 push-ups",
+    "Listen to music",
+    "Take a short nap",
+    "Write in a journal",
+    "Watch a movie"
+]
+
+choice = random.choice(activities)
+print("Today's random activity is:", choice)
